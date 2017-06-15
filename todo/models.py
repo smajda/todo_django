cbf8d8c59a866242db365bc9b1746ef042452169
local <local@local>
2017-06-15 12:54:10 -0500

    About to flush database to update model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class item(models.Model):
+    '''Represents an item on a todo list, such as user, desc, etc.
+    '''
+    title_text = models.CharField(max_length=200)
+    desc_text = models.TextField()
+    impact_text = models.TextField()
+    add_date = models.DateTimeField('date added')
+    due_date = models.DateTimeField('complete by')
+    priority = models.FloatField()
+
+    def __repr__(self):
+        return self.add_date
