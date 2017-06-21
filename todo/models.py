a59fb56a937b08a8123449165fb64e407bb931e7
Noah-Dev <ark_code@protonmail.com>
2017-06-21 18:01:00 -0500

    Finished updating model - now there is start_date, which reflects when the item should begin work

--- todo/models.py
+++ todo/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Item(models.Model):
     '''Represents an item on a todo list, such as user, desc, etc.
     '''
-    title_text = models.CharField(max_length=200, default="")
+    title_text = models.CharField(max_length=200)
     desc_text = models.TextField()
     impact_text = models.TextField()
     start_date = models.DateTimeField()
     due_date = models.DateTimeField()
     priority = models.FloatField()
-    complete = models.BooleanField(default=False)
+    complete = models.BooleanField()
     add_date = models.DateTimeField()
 
     def __repr__(self):
