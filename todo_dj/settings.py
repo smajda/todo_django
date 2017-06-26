3549aae1b5f07672a4321b4c2cfb4f07852f394f
Noah-Dev <ark_code@protonmail.com>
2017-06-26 17:22:59 -0500

    Testing

--- todo_dj/settings.py
+++ todo_dj/settings.py
@@ -128,6 +128,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'todo_dj/static'),
+# )
