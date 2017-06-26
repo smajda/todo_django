c4efd09fc7e617ba870203bdef7f493c72ad7af4
Noah-Dev <ark_code@protonmail.com>
2017-06-26 16:49:32 -0500

    Issues with static - attempting now

--- todo_dj/settings.py
+++ todo_dj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = ['dj-todo.herokuapp.com',
                  'localhost', ]
@@ -119,10 +119,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
-
-STATIC_URL = os.path.join(BASE_DIR, "todo\static")
-STATICFILES_DIRS = [
-]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
