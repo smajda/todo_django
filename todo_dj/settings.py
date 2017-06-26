e08b02d7a2b849512a3777ef817d7f273bd4d984
Noah-Dev <ark_code@protonmail.com>
2017-06-26 17:12:48 -0500

    https://stackoverflow.com/questions/19323513/heroku-django-oserror-no-such-file-or-directory-app-myappname-static

--- todo_dj/settings.py
+++ todo_dj/settings.py
@@ -129,5 +129,5 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'todo_dj/static'),
 )
