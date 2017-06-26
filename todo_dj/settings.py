3a07e324a308d35c52f334970ccca500994c753a
Noah-Dev <ark_code@protonmail.com>
2017-06-26 16:52:38 -0500

    Removed debug

--- todo_dj/settings.py
+++ todo_dj/settings.py
@@ -23,9 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = secret_key()
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['dj-todo.herokuapp.com',
                  'localhost', ]
 
