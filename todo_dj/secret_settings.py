43f66f12fbf9c6d9e79622c24615e05d755983a9
Noah-Dev <ark_code@protonmail.com>
2017-06-29 20:30:50 -0500

    Fixed issue with secret_settings

--- todo_dj/secret_settings.py
+++ todo_dj/secret_settings.py
@@ -1,6 +1,2 @@
 def secret_key():
-<<<<<<< HEAD
-    return "G[EPR+!pFJF>0-<lvQ9rE|.N9m)}>T>@%f4v)B^1JFA'K_o$)M"
-=======
-    return 'gL2H"oYfk"BG"^I~00b,Eti)`0d?2g9vJAQ5aYnx[U/Oq`n>D'
->>>>>>> 9a671cd198e5fdc037648574ed9e39d1fef5b1c1
+    return "PV~@0PK9+&.Pvh~b.v|WP)LPi<1X*x2O]NrbEu+DW|HB4fIU"
