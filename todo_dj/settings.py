c5aca8b224a813ee388bd2628e88f94c2e8579e9
Noah-Dev <ark_code@protonmail.com>
2017-06-29 20:17:04 -0500

    Added from s3utils import * - not sure if it will work

--- todo_dj/settings.py
+++ todo_dj/settings.py
@@ -14,6 +14,7 @@ import os
 from .secret_settings import secret_key
 import dj_database_url
 from boto.s3.connection import S3Connection
+from s3utils import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
