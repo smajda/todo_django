1a54befa18ea028a81a9519ad6fff57fe7f4457d
Noah-Dev <ark_code@protonmail.com>
2017-07-01 16:46:38 -0500

    Attempting fix - did a collecstatic before pushing

--- todo_dj/staticfiles/todo/item.js
+++ todo_dj/staticfiles/todo/item.js
@@ -1,66 +1,89 @@
 $(function() {
-  $("#json_title_text").text('Please select an item to the right...');
+  id = $( ".item:first" ).get(0).id
+  item_ajax(id)
 });
 
-//Change selected item
-$(".item").click(function () {
-  // $("#todo_item").css('background-color', '#ca0000ff')
-  //$("#todo_item").css('color', '#ca0000ff')
+function item_ajax(item_id) {
+  //Retrive an item from the right-hand side list
 
+  //Animation stuff
   $("#todo_item").fadeTo(400, 0, function() {
     $("#todo_item").delay(400);
     $("#todo_item").fadeTo(400, 1);
   })
-  var id = $(this).get(0).id;
   setTimeout(function(){
     $.ajax({
       url: 'ajax/',
       data: {
-        'id': id
+        'id': item_id
       },
       dataType: 'json',
 
       success: function (data) {
         data = JSON.parse(data);
-        $("#json_title_text").text('Title: ' + data['title_text']);
-        $("#json_desc_text").text('Description: ' + data['desc_text']);
-        $("#json_impact_text").text('Impact: ' + data['impact_text']);
-        $("#json_start_date").text('Start Date: ' + data['start_date']);
+        $("#json_title_text").text(data['title_text']);
+        $("#json_desc_text").text(data['desc_text']);
+        $("#json_impact_text").text(data['impact_text']);
+        $("#json_start_date").text(data['start_date']);
         var due_date = data['due_date'].split('-');
         var due_date = due_date[1] + '/' + due_date[2].slice(0,2) + '/' + due_date[0]
-        $("#json_due_date").text('Due Date: ' + due_date);
-        $("#json_add_date").text('Add Date: ' + data['add_date']);
-        // $("#json_priority").text('Priority: ' + data['priority']);
-
-        //setTimeout(function(){ $("#todo_item").css('background-color', '#ecececff') }, 400)
-        //setTimeout(function(){ $("#todo_item").css('color', 'black') }, 400)
+        $("#json_due_date").text(due_date);
+        $("#json_add_date").text(data['add_date']);
+        //$("#json_complete").text(data['complete']);
+        //"<input type="checkbox" name="complete" value="True" checked> Complete?")
+        var checkbox = $('input[name="item_complete"]')
+        if (data['complete'] == false) {
+          checkbox.prop('checked', false);
+        } else if (data['complete'] == true) {
+          checkbox.prop('checked', true);
+        } else {
+          alert("ERROR WITH CHECKBOX")
+        }
+        checkbox.attr('id', item_id);
       }
     });
   }, 600);
-});
-
-function animate_item() {
-
-  //$("#todo_item").css('background-color', '#ca0000ff')
-  //$("#todo_item").css('color', '#ca0000ff')
-  //$("#todo_item").css('background-color', '#ecececff')
-  //$("#todo_item").css('text-color', 'black')
 }
 
-//Show/hide overlay form for new items
+$(".item").click(function () {
+  //Change selected item
+  var id = $(this).get(0).id;
+  item_ajax(id)
+});
 
+
+$('input[name="item_complete"]').on('change', function () {
+  var checkbox = $('input[name="item_complete"]')
+  var item_id = checkbox.get(0).id;
+  var complete = checkbox.is(':checked');
+  console.log(item_id)
+
+  $.ajax({
+    url: 'complete_item/',
+    data: {
+      'id': item_id,
+      'complete': complete
+    },
+    dataType: 'json',
+
+    success: function (data) {
+      var item_complete_element = $("#" + item_id +".complete_info")
+      if (complete){
+        item_complete_element.html("<b>Complete:</b> <span style='color: green'>True</span>")
+      } else if (!complete){
+        item_complete_element.html("<b>Complete:</b> <span style='color: red'>False</span>")
+      } else {
+        console.log("ERROR WITH CHECKBOX JS")
+      }
+    }
+  });
+
+});
+
+//Show/hide overlay form for new items
 $("#add").click(function (){
   $("#new_item").css('display', 'block');
 });
 $("#cancel_button").click(function (){
   $("#new_item").css('display', 'none');
 });
-
-/*
-// https://stackoverflow.com/questions/2457246/jquery-click-function-exclude-children
-$("#new_item").click(function (){
-  $("#new_item").css('display', 'none');
-}).children().click(function(e) {
-  return false;
-});
-*/
