572520b57a3fdcefc58318c4a68d8a5ca949bbff
Noah-Dev <ark_code@protonmail.com>
2017-07-02 00:00:14 -0500

    Ran static collect

--- todo_dj/staticfiles/todo/nav.js
+++ todo_dj/staticfiles/todo/nav.js
@@ -6,22 +6,21 @@ $("#nav").css('width', show)
 $("#content").css('margin-left', show);
 
 $("#nav_toggle").click(function () {
-  console.log("Hello")
   var nav = $("#nav");
   var content = $("#content");
   var nav_icon = $(".nav_icon");
-  var t = $("#nav_toggle");
+  var nav_toggle = $("#nav_toggle");
   if (nav_expanded) {
     nav.css('width', show)
     content.css('margin-left', show);
     nav_icon.css('padding', '10%');
-    $("#nav_toggle").addClass( "mirror_img" );
+    nav_toggle.toggleClass( "mirror_img" );
     nav_expanded = false;
   } else {
     nav.css('width', hide)
     content.css('margin-left', hide);
     nav_icon.css('padding', '10%');
-    $("#nav_toggle").addClass( "mirror_img" );
+    nav_toggle.toggleClass( "mirror_img" );
     nav_expanded = true;
   }
 });
