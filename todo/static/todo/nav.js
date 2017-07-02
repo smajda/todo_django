bc2dbce5ec30d84f7c03e151a463595f023dbe46
Noah-Dev <ark_code@protonmail.com>
2017-07-01 23:59:54 -0500

    Added some animation to nav toggle - also rotates when not expanded

--- todo/static/todo/nav.js
+++ todo/static/todo/nav.js
@@ -1,3 +1,4 @@
+
 var show = '12%';
 var hide = '3%';
 var nav_expanded = false;
@@ -7,16 +8,19 @@ $("#content").css('margin-left', show);
 $("#nav_toggle").click(function () {
   var nav = $("#nav");
   var content = $("#content");
-  var nav_icon = $(".nav_icon")
+  var nav_icon = $(".nav_icon");
+  var nav_toggle = $("#nav_toggle");
   if (nav_expanded) {
     nav.css('width', show)
     content.css('margin-left', show);
     nav_icon.css('padding', '10%');
+    nav_toggle.toggleClass( "mirror_img" );
     nav_expanded = false;
   } else {
     nav.css('width', hide)
     content.css('margin-left', hide);
     nav_icon.css('padding', '10%');
+    nav_toggle.toggleClass( "mirror_img" );
     nav_expanded = true;
   }
 });
--- todo_dj/staticfiles/todo/nav.js
+++ todo_dj/staticfiles/todo/nav.js
@@ -1,3 +1,4 @@
+
 var show = '12%';
 var hide = '3%';
 var nav_expanded = false;
@@ -5,18 +6,22 @@ $("#nav").css('width', show)
 $("#content").css('margin-left', show);
 
 $("#nav_toggle").click(function () {
+  console.log("Hello")
   var nav = $("#nav");
   var content = $("#content");
-  var nav_icon = $(".nav_icon")
+  var nav_icon = $(".nav_icon");
+  var t = $("#nav_toggle");
   if (nav_expanded) {
     nav.css('width', show)
     content.css('margin-left', show);
     nav_icon.css('padding', '10%');
+    $("#nav_toggle").addClass( "mirror_img" );
     nav_expanded = false;
   } else {
     nav.css('width', hide)
     content.css('margin-left', hide);
     nav_icon.css('padding', '10%');
+    $("#nav_toggle").addClass( "mirror_img" );
     nav_expanded = true;
   }
 });
